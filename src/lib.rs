0cde6efec2b36a1341a75cb0a007e3ec36603c53
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2023-11-25 14:00:09 -0800

    add test for subscribe endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder, web};
 use actix_web::dev::Server;
 
 #[get("/health-check")]
@@ -8,10 +8,22 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct SubscribeFormData {
+    name: String,
+    email: String
+}
+
+#[post("/subscribe")]
+async fn subscribe(form: web::Form<SubscribeFormData>) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .service(health_check)
+            .service(subscribe)
     })
     .listen(listener)?
     .run();
--- tests/healt_check.rs
+++ tests/healt_check.rs
@@ -12,6 +12,51 @@ async fn test_health_check_works() {
     assert!(response.status().is_success())
 }
 
+#[tokio::test]
+async fn test_subscribig_to_newsletter_works() {
+    let client = reqwest::Client::new();
+    let response = client
+        .post(format!("{}/subscribe", app()))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body("name=le%20guin&email=ursula_le_guin%40gmail.com")
+        .send()
+        .await
+        .expect("Failed to send request");
+
+    assert!(response.status().is_success());
+}
+
+#[tokio::test]
+async fn test_subscribig_to_newsletter_with_invalid_data_returns_400() {
+    let client = reqwest::Client::new();
+    let app = app();
+    struct TestCase {
+        payload: String,
+        error: String
+    }
+
+    let test_cases = [
+        TestCase {payload: "name=le%20guin".to_string(), error: "Parse error: missing field `email`.".to_string()},
+        TestCase {payload: "email=ursula_le_guin%40gmail.com".to_string(), error: "Parse error: missing field `name`.".to_string()},
+        TestCase {payload: "".to_string(), error: "missing both name and email".to_string()},
+    ];
+
+    for case in test_cases {
+        let response = client
+            .post(format!("{}/subscribe", app))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(case.payload)
+            .send()
+            .await
+            .expect("Failed to send request");
+
+        assert_eq!(response.status().as_u16(), 400);
+        let body = response.text().await.expect("failed to decode request body");
+        assert_eq!(body, case.error);
+    }
+
+}
+
 fn app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to create a tcp listnener");
     let port = listener.local_addr().expect("Unable to get address of listener").port();
